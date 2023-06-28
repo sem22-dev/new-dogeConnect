418777a051ab563a0989c01d3fa814b221b025d8
Yeite Rilsosing Koireng <53212931+chimmykk@users.noreply.github.com>
2023-06-28 10:43:38 +0530

    Update interactions.tsx
    update

--- app/[tweet]/interactions.tsx
+++ app/[tweet]/interactions.tsx
@@ -10,35 +10,44 @@ export function Interactions( { id } : any){
   return(
         <div className="">
         {/* write a comment */}
-        <div className=" border-b border-darkBorder px-3 md:px-10 bg-bgDarkLint py-5 ">
-            <div className="flex gap-5 items-center">
-                <Image src={session?.user?.image || ''} width={50} height={50} alt='image' className=' rounded-[1000px] cursor-pointer text-[30px]'/> 
-                <input type="text" className="w-full bg-transparent outline-none text-white text-base placeholder:text-textGray" placeholder="Write a comment" />
-                <button
-                         onClick={async () => {
-                            const commentInput = document.querySelector('input[name="comment"]') as HTMLInputElement;
-                            const comment = commentInput ? commentInput.value : '';
-                            
-                            const tweetId = id;
-                      
-                            const response = await fetch('/api/twitter/comment', {
-                              method: 'POST',
-                              headers: {
-                                'Content-Type': 'application/json'
-                              },
-                              body: JSON.stringify({ tweetId, comment })
-                            });
-                      
-                            if (response.ok) {
-                              alert('Comment posted successfully!');
-                            } else {
-                              alert('Error posting comment.');
-                            }
-                          }}
-                >
-                    <IoMdSend className="w-[30px] h-[20px] text-textGray cursor-pointer hover:text-bgPink "/>
-                </button>
-            </div>
+        <div className="border-b border-darkBorder px-3 md:px-10 bg-bgDarkLint py-5">
+  <div className="flex gap-5 items-center">
+    <Image src={session?.user?.image || ''} width={50} height={50} alt='image' className='rounded-[1000px] cursor-pointer text-[30px]'/>
+    <input type="text" id="commentInput" className="w-full bg-transparent outline-none text-white text-base placeholder:text-textGray" placeholder="Write a comment" />
+    <button
+      onClick={async () => {
+        const commentInput = document.getElementById('commentInput') as HTMLInputElement;
+        const comment = commentInput ? commentInput.value : '';
+
+        const tweetId = id;
+
+        try {
+          const response = await fetch('/api/twitter/comment', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ tweetId, comment })
+          });
+
+          if (response.ok) {
+            alert('Comment posted successfully!');
+          } else {
+            alert('Error posting comment.');
+          }
+        } catch (error) {
+          console.error('Error:', error);
+          alert('Error posting comment.');
+        }
+      }}
+    >
+      <IoMdSend className="w-[30px] h-[20px] text-textGray cursor-pointer hover:text-bgPink" />
+    </button>
+  </div>
+
+
+
+         
         </div>
         {/* public comments */}
         <div className=" border-b border-darkBorder px-3 md:px-10 bg-bgDarkLint text-white py-5 rounded-b-xl">
